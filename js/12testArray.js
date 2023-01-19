9b853f5bdc96ff6900823ca786e77cf3323d531b
Yvan Douënel <y.douenel@coopernet.fr>
2023-01-19 12:09:58 +0100

    Array en cours.

---
+++ js/12testArray.js
@@ -0,0 +1,21 @@
+const fruits = ["Cerise", "Banane"];
+//const fruits = new Array("Cerise", "Banane");
+console.log(`fruits `, fruits);
+console.log(`Taille du tableau fruits : `, fruits.length);
+
+fruits.push("Poire");
+console.log(`fruits `, fruits);
+
+console.log(`Valeur du premier élément du tableau fruits`, fruits[0]);
+console.log(
+  `Valeur du dernier élément du tableau fruits`,
+  fruits[fruits.length - 1]
+);
+
+// Parcours du tableau
+fruits.forEach((fruit, index) => {
+  console.log(`valeur correspondant à l'index ${index} : `, fruit);
+});
+
+const shop_list = ["lait", "beurre", "salade"];
+// Créer une nouveau tableau qui encapsule chaque élement du tableau entre les balises <li></li>. Ex :"lait" =>  "<li>lait</li>" Utilisez pour cela la méthode map. Vérifiez si cette méthode est pure ou impure
